c7ca0e0f7f99144e8ee87f8b1fb15260505d726d
Fahdahz <Fahdah.zms@gmail.com>
2025-10-21 17:43:59 +0300

    Initial Commit

---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Fahdah Alsamari on 29/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
